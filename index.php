62fba049ac5684a9d85442e1fd0c429a6ee308fc
mwasee68 <mwasee68@gmail.com>
2017-11-29 20:01:33 +0530

    first

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="newpage.css" rel="stylesheet" type="text/css"/>
+        
+        <title></title>
+    </head>
+    <body>
+        <div class="menu">
+            <?php include_once 'menu.php';?>
+            
+        </div>
+        <div class="slide">
+            <?php include_once 'slider.php';?>
+            </div>
+        <div class="content">
+            <?php include_once 'content.php';?>
+        </div>
+   
+        
+        <script src="js/jquery-3.2.1.slim.min.js" type="text/javascript"></script>
+        <script src="js/popper.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ slider.php
@@ -0,0 +1,18 @@
+
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+        <img class="d-block w-100" src="images/Finallyfoundmy newhome!.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="images/certificate.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+        <img class="d-block w-100" src="images/eau de parfum.png" alt="Third slide">
+    </div>
+      <div class="carousel-item">
+          <img class="d-block w-100" src="images/ummmmhhhhhh!!!yummmyyy.png" alt="Third slide">
+    </div>  
+  </div>
+</div>
+
